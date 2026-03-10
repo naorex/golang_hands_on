6e1dc6fdd83ac5ee7b9278a2294868271ff59a4a
naorex <nao23r@gmail.com>
2026-03-10 12:59:40 +0000

    UPDATE : today's progress

---
+++ src/chap3/3-26/3-26.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello(s string, n int) {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("<%d %s>\n", i, s)
+		time.Sleep(time.Duration(n) * time.Millisecond)
+	}
+}
+
+func main() {
+	go hello("hello", 50) // go routine を用いて、直下のhello()とは別スレッドで実行する
+	hello("bye!", 100)
+}
