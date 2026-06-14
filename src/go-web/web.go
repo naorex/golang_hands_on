b6dd9740995617c8c129df6a94c9393d1a37edca
naorex <nao23r@gmail.com>
2026-06-14 13:25:01 +0000

    UPDATE : today's progress

--- src/go-web/web.go
+++ src/go-web/web.go
@@ -46,12 +46,28 @@ func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
 	}
 }
 
+var flg bool = true
+
 // hello handler
 func hello(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
-	er := tmp.Execute(w, nil)
+
+	item := struct{
+		Flg bool
+		Title string
+		Message string
+		JMessage string
+	}{
+		Flg: flg,
+		Title: "Send values",
+		Message: "This is Sample message.",
+		JMessage: "これはサンプルです",
+	}
+
+	er := tmp.Execute(w, item)
 	if er != nil {
 		log.Fatal(er)
 	}
+	flg = !flg
 }
 
 func main() {
